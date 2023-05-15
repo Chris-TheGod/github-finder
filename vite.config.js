e28ec552146ed6283f2fce1c5a25df2f4bf1dfdf
Kristjan Lääne <xonproject@gmail.com>
2023-05-15 18:29:01 +0300

    added UserList component

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+dotenv.config(); // Load environment variables from .env file
+
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    // Your Vite configuration options
+    // ...
+  };
 });
