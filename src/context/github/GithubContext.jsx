aff5a2daea98a7005b1996549a1e3c019e55b748
Kristjan Lääne <xonproject@gmail.com>
2023-05-15 19:30:34 +0300

    cleaned up the code

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -9,12 +9,13 @@ const GITHUB_TOKEN = import.meta.env.VITE_REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   const fetchUsers = async () => {
+    setLoading();
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -27,6 +28,9 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     });
   };
+
+  //set loading
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
 
   return (
     <GithubContext.Provider
