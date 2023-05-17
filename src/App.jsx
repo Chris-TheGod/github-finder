dc0a92016c7e986c23a008d5b4c3876e82675973
Kristjan Lääne <xonproject@gmail.com>
2023-05-17 17:43:42 +0300

    get single user

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { NotFound } from "./pages/NotFound";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
 import { Alert } from "./components/layout/Alert";
+import { User } from "./pages/User";
 
 // src/App.jsx
 export default function App() {
@@ -22,6 +23,8 @@ export default function App() {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+
                 <Route path="/notfound" element={<NotFound />} />
                 <Route path="/*" element={<NotFound />} />
               </Routes>
