46981e78ce53e1bddfb1fd8ff17110de182347d5
Kristjan Lääne <xonproject@gmail.com>
2023-05-17 18:21:46 +0300

    get user repos

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -39,7 +39,7 @@ export const User = () => {
       <div className="w-full mx-auto lg:w-10/12">
         <div className="mb-4">
           <Link to="/" className="btn btn-ghost">
-            Back to search
+            Back To Search
           </Link>
         </div>
 
@@ -51,11 +51,12 @@ export const User = () => {
               </figure>
               <div className="card-body justify-end">
                 <h2 className="card-title mb-0">{name}</h2>
-                <p className="">{login}</p>
+                <p className="flex-grow-0">{login}</p>
               </div>
             </div>
           </div>
-          <div className="call-span-2">
+
+          <div className="col-span-2">
             <div className="mb-6">
               <h1 className="text-3xl card-title">
                 {name}
@@ -72,12 +73,95 @@ export const User = () => {
                   rel="noreferrer"
                   className="btn btn-outline"
                 >
-                  Visit Github Progile
+                  Visit Github Profile
                 </a>
+              </div>
+            </div>
+
+            <div className="w-full rounded-lg shadow-md bg-base-100 stats">
+              {location && (
+                <div className="stat">
+                  <div className="stat-title text-md">Location</div>
+                  <div className="text-lg stat-value">{location}</div>
+                </div>
+              )}
+              {blog && (
+                <div className="stat">
+                  <div className="stat-title text-md">Website</div>
+                  <div className="text-lg stat-value">
+                    <a
+                      href={`https://${blog}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {blog}
+                    </a>
+                  </div>
+                </div>
+              )}
+              {twitter_username && (
+                <div className="stat">
+                  <div className="stat-title text-md">Twitter</div>
+                  <div className="text-lg stat-value">
+                    <a
+                      href={`https://twitter.com/${twitter_username}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {twitter_username}
+                    </a>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaUsers className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Followers</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {followers}
+              </div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaUserFriends className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Following</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {following}
+              </div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaCodepen className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Public Repos</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {public_repos}
+              </div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <FaStore className="text-3xl md:text-5xl" />
+              </div>
+              <div className="stat-title pr-5">Public Gists</div>
+              <div className="stat-value pr-5 text-3xl md:text-4xl">
+                {public_gists}
               </div>
             </div>
           </div>
         </div>
+
+        {/* <RepoList repos={repos} /> */}
       </div>
     </>
   );
